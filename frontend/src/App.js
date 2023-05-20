d4512c7ab173fe0285519ddb1027bcf79621ab21
xCargan <lgc.carlier@gmail.com>
2023-05-21 00:31:00 +0200

    feat(main page):division in section
    /

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className='main_div'>
-      Page principale
+      <div>
+        Head
+      </div>
+      <Navbar />
+      <div>
+        <div>
+          <section id="section1" class="section">
+            <div>
+              <p>section1</p>
+            </div>
+          </section>
+          <section id="section2" class="section">
+            <div>
+              <p>section2</p>
+            </div>
+          </section>
+          <section id="section3"  >
+            <div>
+              <p>section3</p>
+            </div>
+          </section>
+        </div>
+      </div>
+      <div>
+        footer
+      </div>
     </div>
   )
 }
---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div class="navbar">
+            <ul>
+            <li><a href="#section1">Section 1</a></li>
+            <li><a href="#section2">Section 2</a></li>
+            <li><a href="#section3">Section 3</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
