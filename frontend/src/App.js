717c4655f921355af2d5894e79feeb40b756ae9f
Bedusss <samu.marchal@outlook.fr>
2023-05-23 21:36:24 +0200

    fix(mainpage):navbar
    logo pivert hidde on mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,8 @@ function App() {
     <div className='main_div'>
     <Navbar />
     <img src={PivertNavbar} alt="Pivert-Logo" className="PivertNavbar" />
+    <p id="p1_accueil">Le Patro, ce sont des animatrices et animateurs bénévoles qui s’engagent, prennent des responsabilités et participent à une société plus solidaire. </p>
+    <p id="p2_accueil">Ils consacrent de nombreuses heures à cette passion et se forment pour offrir une animation de qualité.</p>
     <div>
       <section id="accueil" class="section">
         <p id="welcome_on_site">Bienvenue dans le monde du</p>
